68c6dc909cad7bb8dea0a0074a0ae6438915275c
wuang <wa@sxw.cn>
2019-08-21 19:31:45 +0800

    update

--- leetcode/src/main/test/Test.java
+++ leetcode/src/main/test/Test.java
@@ -4,23 +4,11 @@ import q24.Solution;
 public class Test {
 
     public static void main(String[] args) {
-        ListNode head = initListNode();
+        ListNode head = ListNode.initListNode();
         System.out.println(head);
 
         System.out.println("==============================");
         ListNode listNode = new Solution().swapPairsOfficial(head);
         System.out.println(listNode);
-    }
-
-    private static ListNode initListNode() {
-        ListNode firstNode = new ListNode(1);
-
-        int count = firstNode.val;
-        ListNode cur = firstNode;
-        while (count < 7) {
-            cur.next = new ListNode(++count);
-            cur = cur.next;
-        }
-        return firstNode;
     }
 }
